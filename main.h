9a86395349b24bc8390913c865620eea41a542a0
ckenduiwa <kenduiwacollins@gmail.com>
2022-04-19 14:30:46 -0700

    main.h

--- main.h
+++ main.h
@@ -78,7 +78,7 @@ unsigned int convert_o(va_list args, buffer_t *output,
 unsigned int convert_x(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_X(va_list args, buffer_t *output,
-	        unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_S(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_p(va_list args, buffer_t *output,
